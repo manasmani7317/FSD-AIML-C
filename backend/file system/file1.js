a6e0ac0916b6daa17a210989ac36d5233c2ed36b
manasmani <manasmani176@gmail.com>
2025-02-07 09:49:35 +0530

    Change

--- backend/file system/file1.js
+++ backend/file system/file1.js
@@ -1,4 +1,14 @@
+//const fs = require('fs');
+//const data = fs.readFileSync("./data.txt","utf-8");
+//console.log(data.toString());
+//fs.appendFileSync("./data.txt","Work from another file","utf-8");
+
+
 const fs = require('fs');
 const data = fs.readFileSync("./data.txt","utf-8");
-console.log(data.toString());
-
+console.log(data);
+if(data.match("H")){
+    console.log("H is found");
+    const newdata = data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
